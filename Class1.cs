062ab62bcfa4f89dbe378ee53a11d489728c14c5
MrRobin <mrrobinjohannesson@gmail.com>
2022-12-12 17:30:52 +0100

    New build

--- Class1.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace UnityParser
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ UnityParser.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace uParser
+{
+    public static partial class UnityParser
+    {
+        public static bool TryParseVector3(string input, out Vector3 value)
+        {
+            value = Vector3.zero;
+            return false;
+        }
+    }
+}
